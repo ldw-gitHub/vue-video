8ce9bdc0c8ed77bcf8e2ae178ec9256a16552eb5
ldw-gitHub <dawei.liu@wayhe.com>
2018-08-21 19:33:25 +0800

    .

--- src/components/ibody.vue
+++ src/components/ibody.vue
@@ -1,42 +1,42 @@
 <template>
-		<div class="containerBody">
-			<div class="panel panel-info moviepanel">
-				<div class="panel-heading">
-					<h4 class="panel-title">动作</h4>
-				</div>
-				<div class="panel-body" style="width: 100%; padding-top: 5px;">
-					<div id="actionMovie" class="col-md-12 movie_container"></div>
-				</div>
+	<div class="containerBody">
+		<div class="panel panel-info moviepanel">
+			<div class="panel-heading">
+				<h4 class="panel-title">动作</h4>
 			</div>
-
-			<div class="panel panel-primary moviepanel">
-				<div class="panel-heading">
-					<h4 class="panel-title">文艺</h4>
-				</div>
-				<div class="panel-body" style="width: 100%; padding-top: 5px;">
-					<div id="artMovie" class="col-md-12 movie_container"></div>
-				</div>
+			<div class="panel-body" style="width: 100%; padding-top: 5px;">
+				<div id="actionMovie" class="col-md-12 movie_container"></div>
 			</div>
-
-			<div class="panel panel-success moviepanel">
-				<div class="panel-heading">
-					<h4 class="panel-title">搞笑</h4>
-				</div>
-				<div class="panel-body" style="width: 100%; padding-top: 5px;">
-					<div id="funnyMovie" class="col-md-12 movie_container"></div>
-				</div>
-			</div>
-
-			<div class="panel panel-warning moviepanel">
-				<div class="panel-heading">
-					<h4 class="panel-title">惊悚</h4>
-				</div>
-				<div class="panel-body" style="width: 100%; padding-top: 5px;">
-					<div id="ThrillerMovie" class="col-md-12 movie_container"></div>
-				</div>
-			</div>
-
 		</div>
+
+		<div class="panel panel-primary moviepanel">
+			<div class="panel-heading">
+				<h4 class="panel-title">文艺</h4>
+			</div>
+			<div class="panel-body" style="width: 100%; padding-top: 5px;">
+				<div id="artMovie" class="col-md-12 movie_container"></div>
+			</div>
+		</div>
+
+		<div class="panel panel-success moviepanel">
+			<div class="panel-heading">
+				<h4 class="panel-title">搞笑</h4>
+			</div>
+			<div class="panel-body" style="width: 100%; padding-top: 5px;">
+				<div id="funnyMovie" class="col-md-12 movie_container"></div>
+			</div>
+		</div>
+
+		<div class="panel panel-warning moviepanel">
+			<div class="panel-heading">
+				<h4 class="panel-title">惊悚</h4>
+			</div>
+			<div class="panel-body" style="width: 100%; padding-top: 5px;">
+				<div id="ThrillerMovie" class="col-md-12 movie_container"></div>
+			</div>
+		</div>
+
+	</div>
 </template>
 
 <script>
@@ -80,18 +80,16 @@
 							for (var i = 0; i < length; i++) {
 								if (i == 0) {
 									info +=
-										`<div id="videos_${i}" onclick="getVideos(${tmp[i].id})" style="float:left;width:35%;height:75%;">
-											         <a>
-															 <img src="http://${ftpIP}/${tmp[i].imgpath}" style="cursor: pointer;BORDER-TOP-WIDTH: 0px; BORDER-LEFT-WIDTH: 0px; 
+										`<div id="videos_${i}" @click="getVideos(${tmp[i].id})" style="float:left;width:35%;height:75%;">
+											         <a><img src="http://${ftpIP}/${tmp[i].imgpath}" style="cursor: pointer;BORDER-TOP-WIDTH: 0px; BORDER-LEFT-WIDTH: 0px; 
 													 FILTER: progid:DXImageTransform.Microsoft.RevealTrans (duration=2,transition=23); BORDER-BOTTOM-WIDTH: 0px; 
-													 BORDER-RIGHT-WIDTH: 0px;width:95%;height:100%;">
-													     </a>
+													 BORDER-RIGHT-WIDTH: 0px;width:95%;height:100%;"></a>
 											         <p style="cursor: pointer;height:18;width:115px;overflow:hidden;white-space:nowrap;text-overflow:ellipsis;">${tmp[i].title}</p>
 											         <p style="height:18;width:70%;overflow:hidden;white-space:nowrap;text-overflow:ellipsis;" 
 													 title=${tmp[i].description}>${tmp[i].description}</p></div>`;
 								} else {
 									info +=
-										`<div id="videos_${i}" onclick="getVideos(${tmp[i].id})" style="float:left;width:16%;height:45%">
+										`<div id="videos_${i}" @click="getVideos(${tmp[i].id})" style="float:left;width:16%;height:45%">
 													<a><img src="http://${ftpIP}/${tmp[i].imgpath}" style="cursor: pointer;BORDER-TOP-WIDTH: 0px; BORDER-LEFT-WIDTH: 0px; 
 													FILTER: progid:DXImageTransform.Microsoft.RevealTrans (duration=2,transition=23); BORDER-BOTTOM-WIDTH: 0px; 
 													BORDER-RIGHT-WIDTH: 0px;width:90%;height:70%;"></a>
@@ -107,6 +105,9 @@
 				})
 
 
+			},
+			getVideos: function (videoId) {
+				console.log(videoId);
 			}
 		},
 	}
